6488566473772853e2e2aad0af12f986bd8c06f2
Rajdeep Gill <35fear35@gmail.com>
2024-12-23 12:44:20 -0600

    Add documents page and editor component

---
+++ src/app/documents/page.tsx
@@ -0,0 +1,5 @@
+const DocumentsPage = () => {
+  return <div>Hi</div>;
+};
+
+export default DocumentsPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const Home = () => {
   return (
-    <div className='bg-blue-300 flex flex-col min-h-screen items-center justify-center text-8xl'>
-      Hello World!
+    <div className='bg-emerald-200 flex flex-col min-h-screen items-center justify-center text-8xl'>
+      <Link href='/documents/123' className='underline text-blue-400'>
+        Document ID PAGE
+      </Link>
     </div>
   );
 };
