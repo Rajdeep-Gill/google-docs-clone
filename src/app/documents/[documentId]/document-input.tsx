301bdafe77a5bd50c46c354df66fef354f65e10e
Rajdeep Gill <35fear35@gmail.com>
2024-12-25 17:01:10 -0600

    Implement renaming inside of a document, and implement leftover navbar features

--- src/app/documents/[documentId]/document-input.tsx
+++ src/app/documents/[documentId]/document-input.tsx
@@ -1,12 +1,96 @@
-import { BsCloudCheck } from 'react-icons/bs';
+'use client';
+import { BsCloudCheck, BsCloudSlash } from 'react-icons/bs';
+import { Id } from '../../../../convex/_generated/dataModel';
+import React, { useRef, useState } from 'react';
+import { useMutation } from 'convex/react';
+import { api } from '../../../../convex/_generated/api';
+import { useDebounce } from '@/hooks/use-debounce';
+import { toast } from 'sonner';
+import { useStatus } from '@liveblocks/react';
+import { stat } from 'fs';
+import { LoaderIcon } from 'lucide-react';
 
-export const DocumentInput = () => {
+interface DocumentInputProps {
+  title: string;
+  id: Id<'documents'>;
+}
+export const DocumentInput = ({ title, id }: DocumentInputProps) => {
+  const status = useStatus();
+
+  const [value, setValue] = useState(title);
+  const [isPending, setIsPending] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const mutate = useMutation(api.documents.updateById);
+
+  const debouncedUpdate = useDebounce((newValue: string) => {
+    if (newValue === title) return;
+
+    setIsPending(true);
+    mutate({ id, title: newValue })
+      .then(() => toast.success('Document title updated'))
+      .catch(() => toast.error('Failed to update document title'))
+      .finally(() => setIsPending(false));
+  });
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+    debouncedUpdate(newValue);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsPending(true);
+    mutate({ id, title: value })
+      .then(() => {
+        toast.success('Document title updated');
+        setIsEditing(false);
+      })
+      .catch(() => toast.error('Failed to update document title'))
+      .finally(() => setIsPending(false));
+  };
+
+  const showLoader =
+    isPending || status === 'connecting' || status === 'reconnecting';
+  const showError = status === 'disconnected';
+
   return (
     <div className='flex items-center gap-2'>
-      <span className='text-lg px-1.5 cursor-pointer truncate'>
-        Untitled Document
-      </span>
-      <BsCloudCheck />
+      {isEditing ? (
+        <form onSubmit={handleSubmit} className='relative w-fit max-w-50ch'>
+          <span className='invisible whitespace-pre px-1.5 text-lg'>
+            {value || ' '}
+          </span>
+          <input
+            ref={inputRef}
+            value={value}
+            onChange={onChange}
+            onBlur={() => setIsEditing(false)}
+            className='absolute inset-0 text-lg text-black px-1.5 bg-transparent truncate'
+          />
+        </form>
+      ) : (
+        <span
+          onClick={() => {
+            setIsEditing(true);
+            setTimeout(() => {
+              inputRef.current?.focus();
+            }, 0);
+          }}
+          className='text-lg px-1.5 cursor-pointer truncate'
+        >
+          {title}
+        </span>
+      )}
+      {!showError && !showLoader && <BsCloudCheck />}
+      {showLoader && (
+        <LoaderIcon className='animate-spin size-4 text-muted-foreground' />
+      )}
+      {showError && <BsCloudSlash className='size-4' />}
     </div>
   );
 };
--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -41,6 +41,12 @@ import { cn } from '@/lib/utils';
 import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 import { Avatars } from './avatars';
 import { Inbox } from './inbox';
+import { Doc } from '../../../../convex/_generated/dataModel';
+import { useMutation } from 'convex/react';
+import { api } from '../../../../convex/_generated/api';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
+import { RenameDialog } from '@/components/rename-dialog';
 
 const TableGrid = ({
   handleClick,
@@ -101,8 +107,23 @@ const TableGrid = ({
   );
 };
 
-export const Navbar = () => {
+interface NavbarProps {
+  data: Doc<'documents'>;
+}
+
+export const Navbar = ({ data }: NavbarProps) => {
+  const router = useRouter();
   const { editor } = useEditorStore();
+
+  const mutation = useMutation(api.documents.create);
+  const onNewDocument = () => {
+    mutation({ title: 'Untitled', initialContent: '' })
+      .catch(() => toast.error('Faield to create a document'))
+      .then((id) => {
+        toast.success('Document created');
+        router.push(`/documents/${id}`);
+      });
+  };
 
   const insertTable = (rows: number, cols: number) => {
     editor
@@ -127,7 +148,7 @@ export const Navbar = () => {
     const blob = new Blob([JSON.stringify(content)], {
       type: 'application/json',
     });
-    onDownload(blob, 'document.json');
+    onDownload(blob, `${data.title}.json`);
   };
 
   const onSaveHTML = () => {
@@ -137,7 +158,7 @@ export const Navbar = () => {
     const blob = new Blob([content], {
       type: 'text/html',
     });
-    onDownload(blob, 'document.html');
+    onDownload(blob, `${data.title}.html`);
   };
 
   const onSaveText = () => {
@@ -147,7 +168,7 @@ export const Navbar = () => {
     const blob = new Blob([content], {
       type: 'text/plain',
     });
-    onDownload(blob, 'document.txt');
+    onDownload(blob, `${data.title}.txt`);
   };
 
   return (
@@ -157,7 +178,7 @@ export const Navbar = () => {
           <Image src='/logo.svg' alt='logo' width={36} height={36} />
         </Link>
         <div className='flex flex-col'>
-          <DocumentInput />
+          <DocumentInput title={data.title} id={data._id} />
           <div className='flex'>
             <Menubar className='border-none bg-transparent shadow-none h-auto p-0'>
               <MenubarMenu>
@@ -184,16 +205,18 @@ export const Navbar = () => {
                       </MenubarItem>
                     </MenubarSubContent>
                   </MenubarSub>
-                  <MenubarItem>
+                  <MenubarItem onClick={onNewDocument}>
                     <FilePlusIcon className='size-4 mr-2' /> New Document
                   </MenubarItem>
                   <MenubarSeparator />
-                  <MenubarItem>
-                    <FilePenIcon className='size-4 mr-2' /> Rename
-                  </MenubarItem>
-                  <MenubarItem>
-                    <TrashIcon className='size-4 mr-2' /> Delete
-                  </MenubarItem>
+                  <RenameDialog documentId={data._id} initialTitle={data.title}>
+                    <MenubarItem
+                      onClick={(e) => e.stopPropagation()}
+                      onSelect={(e) => e.preventDefault()}
+                    >
+                      <FilePenIcon className='size-4 mr-2' /> Rename
+                    </MenubarItem>
+                  </RenameDialog>
                   <MenubarSeparator />
                   <MenubarItem onClick={() => window.print()}>
                     <PrinterIcon className='size-4 mr-2' /> Print
